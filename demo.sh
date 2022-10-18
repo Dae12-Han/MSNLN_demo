a2373d943db9d2b5f75f2b78801d19d97da98564
Dae12-Han <drakaina10@gmail.com>
2022-10-18 18:24:20 +0200

    Add files via upload

---
+++ demo.sh
@@ -0,0 +1,10 @@
+# MSNLN Training Code
+!python3 main.py --chop --batch_size 4 --model MSNLN_631_3 --scale 2 --patch_size 64 --save MSNLN_631_3_x2 --n_feats 128 --depth 12 --data_train DIV2K --save_models
+
+# MSNLN Test Code
+!python3 main.py --model MSNLN_631_3 --data_test Set5+Set14+B100+Urban100 --data_range 801-900 --scale 2 --n_feats 128 --depth 12 --pre_train ./experiment/MSNLN_631_3_x2/model/model_best.pt --save_results --test_only --chop
+
+소원아 오랜만
+  - 이정한 -
+
+python main.py --model MSNLN_631_3 --data_test Set5+Set14+B100+Urban100 --data_range 801-900 --scale 2 --n_feats 128 --depth 12 --pre_train ./experiment/MSNLN_631_3_x2/model/model_best.pt --save_results --test_only --chop
