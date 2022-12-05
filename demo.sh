6ad47747d15003bc12df68b2619546f1ed419b1b
Dae12-Han <drakaina10@gmail.com>
2022-12-05 10:30:35 +0900

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -3,8 +3,3 @@
 
 # MSNLN Test Code
 !python3 main.py --model MSNLN_631_3 --data_test Set5+Set14+B100+Urban100 --data_range 801-900 --scale 2 --n_feats 128 --depth 12 --pre_train ./experiment/MSNLN_631_3_x2/model/model_best.pt --save_results --test_only --chop
-
-소원아 오랜만
-  - 이정한 -
-
-python main.py --model MSNLN_631_3 --data_test Set5+Set14+B100+Urban100 --data_range 801-900 --scale 2 --n_feats 128 --depth 12 --pre_train ./experiment/MSNLN_631_3_x2/model/model_best.pt --save_results --test_only --chop
